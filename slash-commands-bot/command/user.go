ed20932d00b624c449058045fde86005a4aa81d8
luke92 <lucasjv92@gmail.com>
2022-07-25 13:43:34 -0300

    Add options

--- slash-commands-bot/command/user.go
+++ slash-commands-bot/command/user.go
@@ -20,6 +20,27 @@ var UserCommand = discordgo.ApplicationCommand{
 			Description: "User from server",
 			Required:    true,
 		},
+		{
+			Type:        discordgo.ApplicationCommandOptionInteger,
+			Name:        "amount",
+			Description: "Amount",
+			Required:    true,
+		},
+		{
+			Type:        discordgo.ApplicationCommandOptionString,
+			Name:        "category",
+			Description: "Category of Reward",
+			Required:    true,
+			Choices: []*discordgo.ApplicationCommandOptionChoice{
+				{Name: "Kindness", Value: "kindness"},
+				{Name: "Popularity", Value: "popularity"},
+				{Name: "Brilliance", Value: "brilliance"},
+				{Name: "Modak Fan", Value: "modak_fan"},
+				{Name: "Reference", Value: "reference"},
+				{Name: "Gift", Value: "gift"},
+				{Name: "Other", Value: "other"},
+			},
+		},
 	},
 }
 
@@ -30,6 +51,20 @@ func UserCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	Var("User", i.Interaction.Member)
 	message := fmt.Sprintln("Called by User Id:", i.Interaction.Member.User.ID)
 	message += fmt.Sprintln("Username:", i.Interaction.Member.User.String())
+
+	for _, o := range options {
+		if o.Name == "user" {
+			message += fmt.Sprintln("Receiver:", o.Value.(string))
+		}
+
+		if o.Name == "category" {
+			message += fmt.Sprintln("Category:", o.Value.(string))
+		}
+
+		if o.Name == "amount" {
+			message += fmt.Sprintln("Amount:", int(o.IntValue()))
+		}
+	}
 
 	_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
